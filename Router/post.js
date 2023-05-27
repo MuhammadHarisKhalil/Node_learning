bbd13b9fbe10143afe5b4c2958d56e1fa2fe13fb
haris3474 <haris.khalil@manafatech.com>
2023-05-27 12:39:42 +0500

    comment api

--- Router/post.js
+++ Router/post.js
@@ -73,4 +73,23 @@ router.put("/unlike",requirelogin,(req,res)=>{
     })
 })
 
+router.put("/comment",requirelogin,(req,res)=>{
+    const comment = {
+        text: req.body.text,
+        postedBy : req.user_id
+    }
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{comments:comment}
+    },{
+        new:true
+    }).populate("comments.postedBy","_id name")
+    .exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
 module.exports = router
